4bfd0ea425cadd2ccc051a66b435e1877de08439
oussrh <ousrh7@gmail.com>
2021-11-15 12:35:16 +0100

    add open graph and twitter card

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,58 @@ import { Helmet } from 'react-helmet'
 import Player from "../components/Player"
 import { JsonLd } from '../components/JsonLd'
 
+import Logo from "../imgs/logo.png"
+
 
 export default function Home() {
   return(
       <main>
           <Helmet htmlAttributes={{
             lang: 'en',
-          }}>
+          }}
+          meta={[
+            {
+              name: `description`,
+              content: "Radio Rayhan is a web radio station dedicated to Quran recitation, meaningful songs, Prophets biography, and a variety of lessons and wisdom.",
+            },
+            {
+              property: `og:title`,
+              content: "Radio Rayhan",
+            },
+            {
+              property: `og:description`,
+              content: "Radio Rayhan is a web radio station dedicated to Quran recitation, meaningful songs, Prophets biography, and a variety of lessons and wisdom.",
+            },
+            {
+              property: `og:type`,
+              content: `website`,
+            },
+            {
+              name: 'og:image',
+              content: Logo,
+            },
+            {
+              name: `twitter:card`,
+              content: `summary_large_image`,
+            },
+            {
+              name: `twitter:creator`,
+              content: "Radio Rayhan",
+            },
+            {
+              name: `twitter:title`,
+              content: "Radio Rayhan",
+            },
+            {
+              name: `twitter:description`,
+              content: "Radio Rayhan is a web radio station dedicated to Quran recitation, meaningful songs, Prophets biography, and a variety of lessons and wisdom.",
+            },
+            {
+              name: 'twitter:image',
+              content: Logo,
+            },
+          ]}
+          >
             <html lang="en" />
             <title>Radio Rayhan</title>
             <description>Radio RayhanRadio Rayhan is a web radio station dedicated to Quran recitation, meaningful songs, Prophets biography, and a variety of lessons and wisdom.</description>
@@ -26,7 +71,8 @@ export default function Home() {
                   "https://www.facebook.com/Rayhan.radio",
                   "https://www.instagram.com/radio_rayhan",
                   "https://twitter.com/Radio_Rayhan"
-                ]
+                ],
+                
               }}
             </JsonLd>
           </Helmet>
